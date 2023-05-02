606b070cab4f19934a8b4824de95b1b5502b5724
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2023-05-02 02:58:53 -0300

    Reorganizando Codigo

--- android/app/src/release/java/com/cnit/ReactNativeFlipper.java
+++ android/app/src/release/java/com/cnit/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.joaocruz3108.cnitapp;
+package com.cnit;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
